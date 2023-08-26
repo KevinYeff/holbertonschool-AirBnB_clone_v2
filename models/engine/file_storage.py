cd627b4f7f5dc203a83bf81930e7b49e62f64071
KevinYeff <kevinyeff94@gmail.com>
2023-08-25 21:27:17 -0500

    refactoring: implementing readability in the save() method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,7 +14,8 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        key = obj.to_dict()['__class__'] + '.' + obj.id
+        self.all().update({key: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
