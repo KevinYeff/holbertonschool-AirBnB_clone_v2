3b61cd26e725e7942d05e2469be611867695bc08
KevinYeff <kevinyeff94@gmail.com>
2023-08-25 23:15:21 -0500

    bug: reload method causes bug on count() method in console.py it deletes the content from the file.json every time that console is executed, inside method import (reload) moved to the top of the file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+import models
+from os import path
 
 
 class FileStorage:
@@ -19,6 +21,7 @@ class FileStorage:
 
     def save(self):
         """Saves storage dictionary to file"""
+
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
@@ -28,14 +31,12 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models import classes
-        from os import path
-
-        if path.exists(self.__file_path):
-            with open(self.__file_path, "r", encoding="utf-8") as f:
+        file_path = self.__file_path
+        if path.exists(file_path):
+            with open(file_path, "r", encoding="utf-8") as f:
                 self.__objects = json.load(f)
                 for key, value in self.__objects.items():
                     extract_cls_name = value["__class__"]
-                    check_cls_in_var = classes[extract_cls_name]
+                    check_cls_in_var = models.classes[extract_cls_name]
                     simple_instance = check_cls_in_var(**value)
                     self.__objects[key] = simple_instance
