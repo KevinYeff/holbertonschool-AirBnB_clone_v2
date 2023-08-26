b034d1dee1bcb3a482696a852ae32c0f7096116f
KevinYeff <kevinyeff94@gmail.com>
2023-08-25 20:26:19 -0500

    fix: cleaning reload() method by just importing the variable classes from the init file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,19 +27,8 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
+        from models import classes
 
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
