c2e3749d2439369f27e82c24ed2262b7707dbeca
KevinYeff <kevinyeff94@gmail.com>
2023-09-10 20:20:14 -0500

    update: public instance method named all updated, now accepts on type of class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,9 +10,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+        else:
+            temp_dict = {}
+            for key, value in self.__objects.items():
+                if isinstance(value, cls):
+                    temp_dict[key] = value
+            return temp_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
