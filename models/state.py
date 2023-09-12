a32f431c73703784d203471ba91466e98046d3e1
KevinYeff <kevinyeff94@gmail.com>
2023-09-11 20:29:38 -0500

    update: State class now inherits also from Base new attributes were added also a relationship between state and city when dbstorage is used (at this point the dbstorage engine is not imple- mented (yet))

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+#now inherits also from Base
+from models.base_model import BaseModel, Base
+#importing column ans string for table attributes
+from sqlalchemy import Column, String
+#for relationship
+from models.city import City
+from sqlalchemy.orm import relationship
+
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """ State class 
+    update: now will have new attributes to link to 
+    a MySQL table"""
+    
+    #new class attribute
+    __tablename__ = "states"
+    #class attribute changed
+    name = Column(String(128), nullable=False)
+    #for dbstorage relationship
+    cities = relationship("City", backref="state", cascade="delete")
+    
+    # getter attribute whe filestorage is used instead of dbstorage
+    @property
+    def cities(self):
+        """No documentation yet"""
+        return []    
