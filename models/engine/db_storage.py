ec0e531335b6a986d86a26860fd177347683ac31
KevinYeff <kevinyeff94@gmail.com>
2023-09-11 23:50:00 -0500

    update: engine created using enviromental vars

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 """New engine DBStorage"""
+# to get enviromental variables
+from os import environ
+# to create the engine
+from sqlalchemy import create_engine
+# create_engine syntaxis: [dialect]+[driver]://[username]:[password]@[host]:[port]/[database]
+
 
 class DBStorage():
     """New class that represents an storage engine and has the
@@ -12,4 +18,9 @@ class DBStorage():
         """This method creates the engine, the engine must be linked
         to the MySQL database and user created in previus tasks
         hbnb_dev and hbnb_dev_db"""
-        pass
+        # creating engine using the enviromental variables (retrieving values vía enviromental variables)
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(environ.get("HBNB_MYSQL_USER"),
+                                                                              environ.get("HBNB_MYSQL_PWD"),
+                                                                              environ.get("HBNB_MYSQL_HOST"),
+                                                                              environ.get("HBNB_MYSQL_DB")))
+        
