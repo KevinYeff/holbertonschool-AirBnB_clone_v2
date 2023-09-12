8eb437807df0f74046b728226438d9dfafba083b
KevinYeff <kevinyeff94@gmail.com>
2023-09-11 19:25:38 -0500

    update: City class now also inherits from Base Some of the attributes are changed due to task requierments

--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name 
+    update: now will have new attributes to link to 
+    a MySQL table"""
+    
+    # new class attribute:
+    __tablename__ = "cities"
+    # attributes order changed
+    name = Column(String(128), nullalble=False)
+    state_id = Column(String(60), ForeignKey("state.id"), nullable=False,)
+    
