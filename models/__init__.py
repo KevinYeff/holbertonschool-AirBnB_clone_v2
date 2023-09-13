df9c23500f10dd58ae6b5d534d5d5863654a2fcb
KevinYeff <kevinyeff94@gmail.com>
2023-09-13 04:44:24 -0500

    fix: cleaning for warnings

--- models/__init__.py
+++ models/__init__.py
@@ -1,33 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from .base_model import BaseModel
-from .user import User
-from .state import State
-from .city import City
-from .amenity import Amenity
-from .place import Place
-from .review import Review
+
 from os import environ
-
-classes = {
-    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-    'State': State, 'City': City, 'Amenity': Amenity,
-    'Review': Review
-}
-
-dot_cmds = ['all', 'count', 'show', 'destroy', 'update', 'create']
-
-types = {
-    'number_rooms': int, 'number_bathrooms': int,
-    'max_guest': int, 'price_by_night': int,
-    'latitude': float, 'longitude': float
-}
 
 if environ.get("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
 storage.reload()
