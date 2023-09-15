12581d671f1b261f244729886332fcb11aea9b31
KevinYeff <kevinyeff94@gmail.com>
2023-09-14 23:30:56 -0500

    fix: conditional when db storage is used removed

--- models/state.py
+++ models/state.py
@@ -16,18 +16,18 @@ class State(BaseModel, Base):
     update: now will have new attributes to link to 
     a MySQL table"""
     #when using "dbstorage"
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
-        #new class attribute
-        __tablename__ = "states"
-        #class attribute changed
-        name = Column(String(128), nullable=False)
-        #for dbstorage relationship
-        cities = relationship("City", backref="state", cascade="delete")
+    #if environ.get('HBNB_TYPE_STORAGE') == 'db':
+    #new class attribute
+    __tablename__ = "states"
+    #class attribute changed
+    name = Column(String(128), nullable=False)
+    #for dbstorage relationship
+    cities = relationship("City", backref="state", cascade="delete")
     
     #when using another type storage
     # the the enviromental variable is not implemented yet but is on top of the
     # project requirements
-    elif environ.get('HBNB_TYPE_STORAGE') != 'db':
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
         # getter attribute when filestorage is used instead of dbstorage
         @property
         def cities(self):
@@ -38,12 +38,12 @@ class State(BaseModel, Base):
             # storage variableimported
             from models import storage
             #implement an empty list
-            city_lsit = []
+            city_list = []
             # iterate through the City instances
             for city in storage.all(City).values():
                 #in the previous task we update the state_id (class attribute)
                 if city.state_id == self.id:
                     # Add cities that are related to state (current) to the list
-                    city_lsit.append(city)
+                    city_list.append(city)
             # return that list        
-            return city_lsit
+            return city_list
