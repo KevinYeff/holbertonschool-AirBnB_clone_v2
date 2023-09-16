67cf4eb31c684d1f3ec06f2eaf1d7d40251aaf2d
KevinYeff <kevinyeff94@gmail.com>
2023-09-16 02:35:42 -0500

    fix: importing models to call storage Added a foreignkey to 2nd column of sqlalchemy table instance latitude and longitude now can be null Setter to controll objects added

--- models/place.py
+++ models/place.py
@@ -5,13 +5,14 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 #update relationship
 from sqlalchemy.orm import relationship
 from os import environ
-from models import storage
+import models
 
 place_amenity = Table("place_amenity", Base.metadata,
                       Column("place_id", String(60), ForeignKey("places.id"), 
                              primary_key=True,
                              nullable=False),
                       Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
                              primary_key=True,
                              nullable=False))
 
@@ -26,8 +27,8 @@ class Place(BaseModel, Base):
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     #Update: add class attribute
     reviews = relationship("Review", backref="place", cascade="all")
     #Update: new class attribute
@@ -54,7 +55,7 @@ class Place(BaseModel, Base):
             from models.review import Review
             
             review_objs = []
-            for review in storage.all(Review).values():
+            for review in models.storage.all(Review).values():
                 if review.place_id == self.id:
                     review_objs.append(review)
             return review_objs
@@ -64,8 +65,19 @@ class Place(BaseModel, Base):
         def amenities(self):
             from models.amenity import Amenity
             amenity_objs = []
-            for amenity in storage.all(Amenity).values():
+            for amenity in models.storage.all(Amenity).values():
                 if amenity.place_id == self.amenity_ids:
                     amenity_objs.append(amenity)
             return amenity_objs
-                
+        
+        # controlling what is going to be inside the amenity_ids list
+        # only Amenity objects otherwise do nothing     
+        @amenities.setter
+        
+        def amenities(self, value):
+            """Setter for the amenities attribute."""
+            from models.amenity import Amenity
+            
+            if isinstance(value, Amenity):
+                self.amenity_ids.append(value.id)
+        
