aaaa106414b631065cba8f01cab0a989ee088c1a
KevinYeff <kevinyeff94@gmail.com>
2023-09-21 02:59:58 -0500

    feat: debuggin, not finding relationship between tables

--- models/user.py
+++ models/user.py
@@ -12,7 +12,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name= Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)    
-    places = relationship("Place", backref="user", cascade="all")
+    #places = relationship("Place", backref="user", cascade="all")
     #update: add class attribute
     reviews = relationship("Review", backref="user", cascade="all")
     """Attributes before update. This class defines a user by various attributes
