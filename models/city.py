7b65588cd23eb9ae20db29d557a7e9299568c486
KevinYeff <kevinyeff94@gmail.com>
2023-09-25 07:31:18 -0500

    fix: console now works fine

--- models/city.py
+++ models/city.py
@@ -5,16 +5,22 @@ from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from os import environ
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name 
-    update: now will have new attributes to link to 
-    a MySQL table"""
-    
-    # new class attribute:
-    __tablename__ = "cities"
-    # attributes order changed
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    #places = relationship("Place", backref="cities", cascade="delete")
-    
+if environ.get('HBNB_TYPE_STORAGE') == 'db':
+    class City(BaseModel, Base):
+        """ The city class, contains state ID and name 
+        update: now will have new attributes to link to 
+        a MySQL table"""
+        
+        # new class attribute:
+        __tablename__ = "cities"
+        # attributes order changed
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
+else: 
+    class City(BaseModel):
+        name = ""
+        state_id = ""
+        
--- models/user.py
+++ models/user.py
@@ -3,21 +3,26 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import environ
 
-class User(BaseModel, Base):
-    """This class defines a User with the new following class attributes"""
-    
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name= Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)    
-    #places = relationship("Place", backref="user", cascade="all")
-    #update: add class attribute
-    reviews = relationship("Review", backref="user", cascade="all")
-    """Attributes before update. This class defines a user by various attributes
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
-    """
+if environ.get('HBNB_TYPE_STORAGE') == 'db':
+    class User(BaseModel, Base):
+        """This class defines a User with the new following class attributes"""
+        
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name= Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)    
+        places = relationship("Place", backref="user", cascade="all")
+        #update: add class attribute
+        reviews = relationship("Review", backref="user", cascade="all")
+else:
+    class User(BaseModel):
+        
+        """Attributes before update. This class defines a user by various attributes"""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
+        
